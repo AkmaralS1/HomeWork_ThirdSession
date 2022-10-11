dc0de29cf7c48f7a09a4b5b92da7e8fe272e4d8a
Akmaral <axma.mail@gmail.com>
2022-10-11 15:39:47 +0600

    Задача 21

---
+++ Task002/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 21 Напишите программу которая принимает на вход координаты 
+//  
+Console.Clear();
+Console.Write("Введите координату х1: ");
+double x1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату y1: ");
+double y1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату c1: ");
+double c1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату х2: ");
+double x2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату y2: ");
+double y2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату c2: ");
+double c2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine(Math.Sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2) + (c1 - c2) * (c1 - c2)));
